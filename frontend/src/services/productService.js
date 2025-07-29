c31708ad6881d4da91dbf970abc264b007a03549
Aakib-hotelwala <hotelwalaaakib721@gmail.com>
2025-07-29 20:27:13 +0530

    backend url added

--- frontend/src/services/productService.js
+++ frontend/src/services/productService.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/products",
+  baseURL: BASE_URL,
 });
 
 export const fetchProducts = (params = {}) => API.get("/", { params });
