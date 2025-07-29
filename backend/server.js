8a9f20f29533d8cfce4efab438c47f72a7e710a0
Aakib-hotelwala <hotelwalaaakib721@gmail.com>
2025-07-29 20:41:07 +0530

    Fix Cors Problem

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,20 @@ const app = express();
 
 ConnectDB();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://shelf-track.vercel.app",
+];
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
